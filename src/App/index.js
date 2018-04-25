3836e9c7329d516ac65f711254130d8786c4fe7d
Paulo Menezes <paulo.hgmenezes@gmail.com>
2018-04-24 22:49:40 -0300

    Initial structure with Bulma

---
+++ src/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Hello World</h1>
+          <p className="subtitle">
+            My first website with <strong>Bulma</strong>
+          </p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default App;
